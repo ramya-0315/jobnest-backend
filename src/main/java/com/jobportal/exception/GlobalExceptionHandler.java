53aa7fceb0a98408998a03a11e086a29b17f71e1
Ramya <ramyacse2024@gmail.com>
2025-05-31 21:52:54 +0530

    Added GlobalExceptionHandler class

---
+++ src/main/java/com/jobportal/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.jobnest.backend.config; // update this with your actual package
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Catch all unhandled exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>("Something went wrong. Please try again later.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
